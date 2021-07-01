7d12be58066ba39ad56e311f9f6fc9b7cba7f466
Jude Busarello <jude.busarello@vistarmedia.com>
2021-07-01 17:28:53 -0400

    Basic Framework For Windows95 Theme'd Site
      * Added Taskbar at bottom of screen
      * Added Link to linkedin
      * Added Link to github
      * Added Link to instagram
      * Added 2 modals for future improvements
      * Changed Background Color to Win95 Green

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '@react95/icons/icons.css';
+import { List } from '@react95/core';
+import { Modal } from '@react95/core';
+import { TaskBar } from '@react95/core';
+import { ThemeProvider } from '@react95/core';
+import { useState } from 'react';
+
 
 function App() {
+  const [resumeOpen, setResumeModalState] = useState(false)
+  const [pictureOpen, setPictureModalState] = useState(false)
+
+  const taskbarList = (
+    <List>
+      <List.Item onClick={() => {setResumeModalState(true)}}>
+        Resume.pdf
+      </List.Item>
+      <List.Divider/>
+      <List.Item onClick={() => {setPictureModalState(true)}}>
+        FamilyPicture.jpeg
+      </List.Item>
+      <List.Divider/>
+      <List.Item onClick={() => {window.open('https://www.github.com/judebusarello')}}>
+        Github
+      </List.Item>
+      <List.Divider/>
+      <List.Item onClick={() => {window.open('https://www.linkedin.com/in/judebusarello')}}>
+        LinkedIn
+      </List.Item>
+      <List.Divider/>
+      <List.Item onClick={() => {window.open('https://www.instagram.com/judebusarello')}}>
+        Instagram
+      </List.Item>
+    </List>
+  )
+
+  const centeredWindowStyle = {
+    display: "flex",
+    'align-items': "center",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider>
+      {resumeOpen && (
+        <Modal
+	  style={centeredWindowStyle}
+	  title="resume.txt"
+  	  closeModal={() => {setResumeModalState(false)}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    	  Still Working On This, My Bad
+        </Modal>
+      )}
+      {pictureOpen && (
+        <Modal
+	  style={centeredWindowStyle}
+	  title="picture.jpeg"
+  	  closeModal={() => {setPictureModalState(false)}}
+        >
+    	  Gonna be a deep fried version of my family
+        </Modal>
+      )}
+      <TaskBar list={taskbarList} />
+    </ThemeProvider>
+  )
 }
 
 export default App;
